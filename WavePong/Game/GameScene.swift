73ef0f9d866c7924f994e567cf6643b2be2f87fe
Lorena Vilaça <lcjbv2@cin.ufpe.br>
2023-08-03 17:43:20 -0300

    fix(physicDetection): resolving error in xcodeproject

--- WavePong/Game/GameScene.swift
+++ WavePong/Game/GameScene.swift
@@ -32,42 +32,7 @@ class GameScene: SKScene {
         ball.run(SKAction.applyImpulse(CGVector(dx: 15, dy: 15), duration: 1))
         cloud.run(SKAction.move(to: CGPoint(x: self.frame.midX, y: self.frame.midY + 100), duration: 20))
     }
-    
-<<<<<<< HEAD
-=======
-    // MARK: Colisions
-    func didBegin(_ contact: SKPhysicsContact) {
-        let ballCategory: UInt32 = 1 // BitMask da bola
-        let paddleCategory: UInt32 = 2 // BitMask da raquete
         
-        if (contact.bodyA.categoryBitMask & ballCategory) == ballCategory {
-            // Colisão entre a bola e a raquete
-            if (contact.bodyB.categoryBitMask & paddleCategory) == paddleCategory {
-                // Bola colidiu com a raquete
-                score += 1
-                scoreLabel.text = "Score: \(score)"
-                
-                impactFeedbackGenerator.impactOccurred()
-            }
-        } else if (contact.bodyB.categoryBitMask & ballCategory) == ballCategory {
-            // Colisão entre a bola e a raquete
-            if (contact.bodyA.categoryBitMask & paddleCategory) == paddleCategory {
-                // Bola colidiu com a raquete
-                score += 1
-                scoreLabel.text = "Score: \(score)"
-                
-                impactFeedbackGenerator.impactOccurred()
-            }
-        }
-    }
-    
-    override func update(_ currentTime: TimeInterval) {
-        let position = newBall.position
-        let size = frame.size
-        soundManager.updateAudioOrientation(ballPosition: position, frameSize: size)
-    }
->>>>>>> develop
-    
 }
 
 extension GameScene: GameSceneProtocol {
