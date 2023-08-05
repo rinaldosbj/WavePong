b368dd4a67b39b71d9c12d498d9b70e75377af88
Rinaldo Jr <107890806+RinaldoJr4@users.noreply.github.com>
2023-08-05 12:24:31 -0300

    feature(Menu): creating MenuView and ConfigurationView

---
+++ WavePong/ConfigurationView.swift
@@ -0,0 +1,20 @@
+//
+//  ConfigurationView.swift
+//  WavePong
+//
+//  Created by rsbj on 05/08/23.
+//
+
+import SwiftUI
+
+struct ConfigurationView: View {
+    var body: some View {
+        Text("Configurações")
+    }
+}
+
+struct ConfigurationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfigurationView()
+    }
+}
