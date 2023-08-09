6fda98874015dedff0bd81e2a1fa29b153182c66
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-08-09 10:19:51 -0300

    feature(InstitutoDoCego): preparing Player for testing in the future

--- WavePong/Game/Player.swift
+++ WavePong/Game/Player.swift
@@ -10,12 +10,16 @@ import Foundation
 /// Class responsable for persisting and updating user info about the app
 class Player: PlayerProtocol {
     
-    private struct Constants {
-        static var hasSeenOnboarding = "hasSeenOnboarding"
-        static var userTopSocre = "userTopScore"
+    init(defaults: UserDefaultable = UserDefaults.standard) {
+        self.defaults = defaults
     }
     
-    private let defaults = UserDefaults.standard
+    private struct Constants {
+        static var hasSeenOnboarding = "hasSeenOnboarding"
+        static var userTopScore = "userTopScore"
+    }
+    
+    private let defaults: UserDefaultable
     
     ///  Informs if the user already seen onboarding
     var shouldShowOnborading: Bool {
@@ -24,7 +28,7 @@ class Player: PlayerProtocol {
     
     ///  Informs the highest score achieved by the player
     var userTopScore: Int {
-        return defaults.integer(forKey: Constants.userTopSocre)
+        return defaults.integer(forKey: Constants.userTopScore)
     }
     
     /// Once player finishes onboarding, this methods must be called so the app won't show onboading every launch
@@ -34,11 +38,24 @@ class Player: PlayerProtocol {
     
     /// Must be called if score of game was higher than the previous top score. Updates
     func updateTopScore(NewTopScore score: Int) {
-        defaults.set(score, forKey: Constants.userTopSocre)
+        defaults.set(score, forKey: Constants.userTopScore)
     }
 
 }
 
+extension UserDefaults: UserDefaultable { }
+
+
+protocol UserDefaultable {
+    func bool(forKey: String) -> Bool
+    
+    func integer(forKey: String) -> Int
+    
+    func set(_ value: Bool, forKey defaultName: String)
+    
+    func set(_ value: Int, forKey defaultName: String)
+    
+}
 
 /// Defines a standard interface for Player Class
 protocol PlayerProtocol {
