c3a75062c4f551714d0c1b168ef6c56a5f0b0575
Lorena Vilaça <55454910+LorenaVilaca@users.noreply.github.com>
2023-08-18 17:16:31 -0300

    Revert "Feature/notifications"

--- WavePong/PongApp.swift
+++ WavePong/PongApp.swift
@@ -9,32 +9,6 @@ import SwiftUI
 
 @main
 struct PongApp: App {
-<<<<<<< HEAD
-    
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    @Environment(\.scenePhase) private var scenePhase
-    @ObservedObject var notificationManager = NotificationManager.shared
-    
-    var body: some Scene {
-        
-        WindowGroup {
-            NavigationView {
-                MenuView()
-            }
-        }.onChange(of: scenePhase) { newScenePhase in
-            switch newScenePhase {
-            case .active:
-                notificationManager.clearNotifications(notificationIdentifier: "Inactivity")
-                print("App is active")
-            case .inactive:
-                notificationManager.scheduleNotifications()
-                print("App is inactive")
-            case .background:
-                print("App is in background")
-            @unknown default:
-                print("Oh - interesting: I received an unexpected new value.")
-            }
-=======
     var player = Player.shared
     var body: some Scene {
         
@@ -48,7 +22,6 @@ struct PongApp: App {
                     }
                 }
                 .navigationViewStyle(.stack)
->>>>>>> develop
         }
     }
 }
