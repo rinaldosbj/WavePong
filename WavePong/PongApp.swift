1b6e798dc3d4616fcafd3fcac494242fdec2ba3f
Lorena Vilaça <lcjbv2@cin.ufpe.br>
2023-08-21 17:00:23 -0300

    fix(pongApp): adjust in app.swift

--- WavePong/PongApp.swift
+++ WavePong/PongApp.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct PongApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @Environment(\.scenePhase) private var scenePhase
+    @ObservedObject var notificationManager = NotificationManager.shared
     var player = Player.shared
     var body: some Scene {
         
@@ -22,7 +26,19 @@ struct PongApp: App {
                 }
             }
             .navigationViewStyle(.stack)
+        }.onChange(of: scenePhase) { newScenePhase in
+            switch newScenePhase {
+            case .active:
+                notificationManager.clearNotifications(notificationIdentifier: "Inactivity")
+                print("App is active")
+            case .inactive:
+                notificationManager.scheduleNotifications()
+                print("App is inactive")
+            case .background:
+                print("App is in background")
+            @unknown default:
+                print("Oh - interesting: I received an unexpected new value.")
+            }
         }
     }
 }
-
