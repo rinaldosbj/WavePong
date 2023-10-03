6c91d7000ab88e707206d651615af27b58832d23
Rinaldo Jr <107890806+RinaldoJr4@users.noreply.github.com>
2023-10-03 17:38:53 -0300

    feature(newOnboarding): integrating new scenes with onboardign

---
+++ WavePong/Views/Onboarding/VolumeView.swift
@@ -0,0 +1,18 @@
+//
+//  VolumeView.swift
+//  WavePong
+//
+//  Created by rsbj on 03/10/23.
+//
+
+import SwiftUI
+
+struct VolumeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VolumeView()
+}
