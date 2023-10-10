a4da699c83742131417a52d645312fa35af67237
Lorena Vilaça <lcjbv2@cin.ufpe.br>
2023-10-10 15:48:00 -0300

    feature(storeView): alterations in store view

---
+++ WavePong/Views/StoreView.swift
@@ -0,0 +1,8 @@
+//
+//  StoreView.swift
+//  WavePong
+//
+//  Created by LoreVilaca on 27/09/23.
+//
+
+import Foundation
---
+++ WavePong/Views/UI Components/StoreCard.swift
@@ -0,0 +1,8 @@
+//
+//  StoreCard.swift
+//  WavePong
+//
+//  Created by LoreVilaca on 27/09/23.
+//
+
+import Foundation
